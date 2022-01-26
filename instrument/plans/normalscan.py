a2621d6c49b04905deb9da6e9854f3519fedbb57
jwkim-anl <jwkim@anl.gov>
2022-01-26 14:19:12 -0600

    white space testing

--- instrument/plans/normalscan.py
+++ instrument/plans/normalscan.py
@@ -1,4 +1,3 @@
 def scan1(ctime=1):
     scaler2.stage_sigs["preset_time"] = ctime
     yield from bp.scan([pilatus100k,scaler2,pilatus100k.stats5.total], psic.phi, 22, 23, 5)
-                                                               
