f2dd8f98944427b74db20d68ed925cbbba5d40a7
Gilberto Fabbris <gfabbris@anl.gov>
2022-02-09 19:22:43 -0600

    tweaks to plot_roi

--- instrument/devices/pilatus_100k.py
+++ instrument/devices/pilatus_100k.py
@@ -1,15 +1,19 @@
-from ophyd import ADComponent
-from ophyd import ImagePlugin
-from ophyd import PilatusDetector
-from ophyd import SingleTrigger
-from ophyd.areadetector.plugins import HDF5Plugin_V34 as HDF5Plugin
-from ophyd.areadetector.plugins import OverlayPlugin_V34 as OverlayPlugin
-from ophyd.areadetector.plugins import ROIPlugin_V34 as ROIPlugin
-from ophyd.areadetector.plugins import StatsPlugin_V34 as StatsPlugin
+from ophyd import (
+    ADComponent,
+    ImagePlugin,
+    PilatusDetector,
+    SingleTrigger
+)
+from ophyd.areadetector.plugins import (
+    HDF5Plugin_V34 as HDF5Plugin,
+    OverlayPlugin_V34 as OverlayPlugin,
+    ROIPlugin_V34 as ROIPlugin,
+    StatsPlugin_V34 as StatsPlugin
+)
 from ophyd.areadetector.filestore_mixins import (
     FileStoreHDF5SingleIterativeWrite
 )
-import os
+from os.path import join
 
 PILATUS_FILES_ROOT = "/home/det/6IDB"
 BLUESKY_FILES_ROOT = "/home/beams/USER6IDB/Data"
@@ -36,8 +40,8 @@ class MyPilatusDetector(SingleTrigger, PilatusDetector):
     hdf1 = ADComponent(
         MyHDF5Plugin,
         "HDF1:",
-        write_path_template=os.path.join(PILATUS_FILES_ROOT, TEST_IMAGE_DIR),
-        read_path_template=os.path.join(BLUESKY_FILES_ROOT, TEST_IMAGE_DIR),
+        write_path_template=join(PILATUS_FILES_ROOT, TEST_IMAGE_DIR),
+        read_path_template=join(BLUESKY_FILES_ROOT, TEST_IMAGE_DIR),
     )
     over1 = ADComponent(OverlayPlugin, 'Over1:')
     roi1 = ADComponent(ROIPlugin, 'ROI1:')
@@ -51,6 +55,9 @@ class MyPilatusDetector(SingleTrigger, PilatusDetector):
     stats5 = ADComponent(StatsPlugin, 'Stats5:')
 
     def default_settings(self):
+        """
+        Set the default Pilatus configuration.
+        """
         # Enter all the important default settings here.
         self.cam.image_mode.put("Single")
         self.cam.num_images.put(1)
@@ -63,13 +70,27 @@ class MyPilatusDetector(SingleTrigger, PilatusDetector):
         self.hdf1.file_template.put("%s%s_%5.5d.h5")
         self.hdf1.auto_save.put(0)
 
-    # Example of roi config.
-    def plot_roi1(self):
-        self.stats1.total.kind = "hinted"  # Is this signal correct?
-        self.stats2.total.kind = "normal"
-        self.stats3.total.kind = "normal"
-        self.stats4.total.kind = "normal"
+    def plot_roi(self, values=[1, 2, 3, 4, 5]):
+        """
+        Select ROI stats to be plotted during scan.
+
+        Parameters
+        ----------
+        values: int or iterable, optional
+            ROI index or list of ROI indexes to be plotted. Defaults to plot
+            all ROIs.
+        """
+        if isinstance(values, (int, float)):
+            values = [int(values)]
+
+        for name in self.component_names:
+            if "stats" in name:
+                kind = "hinted" if int(name[-1]) in values else "normal"
+                attr = getattr(self, name)
+                attr.total.kind = kind
 
 
 pilatus100k = MyPilatusDetector("s6_pilatus:", name="pilatus100k")
 pilatus100k.hdf1.stage_sigs["auto_save"] = 1
+pilatus100k.plot_roi()
+pilatus100k.default_settings()
